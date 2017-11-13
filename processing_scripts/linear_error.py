17a15137821dfeee6e5b200516ffc431e67f7849
ShrimCity <tylerwhughes91@gmail.com>
2017-11-13 15:04:15 -0800

    numpy arrays for trajectoreies, other tracking array

--- processing_scripts/linear_error.py
+++ processing_scripts/linear_error.py
@@ -2,43 +2,44 @@ from numpy import sqrt
 
 
 def compute_linear_error(trajectories, Nf):
+    # Computes total error with a linear model
+    #   inputs:
+    #     - trajectories: list of numpy arrays with indeces i,j for frame index and [x,y,others_list elements], respectively
+    #     - Nf: number of frames to observe before computing error
+    #   outputs:
+    #     - error : sqrt of sum of square loss (normalized by trajectory length)
     dataset_error = 0.0
-    traj_errors = {}
-
-    for pID, traj in trajectories.iteritems():
-        num_frames = len(traj)
+    for traj in trajectories:
+        num_frames,num_people = traj.shape
         if num_frames <= Nf:
-            continue
-        else:
-            t_prev = traj[Nf-1][0]
-            x_prev = traj[Nf-1][1]
-            y_prev = traj[Nf-1][2]        
-            delta_t = traj[Nf-1][0] - traj[Nf-2][0]
-            delta_x = traj[Nf-1][1] - traj[Nf-2][1]
-            delta_y = traj[Nf-1][2] - traj[Nf-2][2]
-            vel_x = delta_x/delta_t
-            vel_y = delta_y/delta_t
+            raise ValueError("Nf needs to be shorter than the number of frames in the trajectory")
+            return 0
+        if Nf < 3:
+            raise ValueError("Nf needs to be greater than 3 for prediction")
+            return 0
+        x_prev = traj[Nf-1][0]
+        y_prev = traj[Nf-1][1]        
+        delta_x = traj[Nf-1][0] - traj[Nf-2][0]
+        delta_y = traj[Nf-1][1] - traj[Nf-2][1]
+        
+        traj_error = 0.0
+        for j in range(Nf,num_frames):
+            x = traj[j][0]
+            y = traj[j][1]
             
-            traj_error = 0.0
-            for j in range(Nf,num_frames):
-                t = traj[j][0]
-                x = traj[j][1]
-                y = traj[j][2]
-                
-                x_pred = x_prev + vel_x*(t-t_prev)
-                y_pred = y_prev + vel_y*(t-t_prev)
-                
-                # note, this is using the initial linear model as the predictor.
-                x_prev = x_pred
-                y_prev = y_pred
-                t_prev = t
-                
-                # may also use new x information to predict
-                x_prev = x
-                y_prev = y
-                t_prev = t   
-                error = sqrt((x-x_pred)**2 + (y-y_pred)**2)
-                traj_error += error
-            traj_errors[pID] = traj_error/(num_frames-Nf)
-            dataset_error += traj_error
+            x_pred = x_prev + delta_x
+            y_pred = y_prev + delta_y
+            
+            # note, this is using the initial linear model as the predictor.
+            x_prev = x_pred
+            y_prev = y_pred
+            
+            # may also use new x information to predict
+            x_prev = x
+            y_prev = y
+
+            error = sqrt((x-x_pred)**2 + (y-y_pred)**2)
+            traj_error += error
+
+        dataset_error += traj_error
     return dataset_error/len(trajectories)
